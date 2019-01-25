1d3f7ae2b4d5f6443543149d30066b7b3d8934fd
willstenzel-blocktest <46688425+willstenzel-blocktest@users.noreply.github.com>
2019-01-25 15:47:59 -0500

    Create Public.sol

---
+++ Public.sol
@@ -0,0 +1,28 @@
+pragma solidity 0.4.25;
+
+contract Public {
+
+    function using_public(uint[] a) public returns (uint) {
+        return a[10];
+    }
+}
+
+contract Test_Public_Internal is Public {
+    
+    function test_public_internal() {
+        uint[] memory a = new uint[](200);
+        a[10] = 8;
+        using_public(a);
+    }
+}
+    
+contract Test_Public_External {
+    
+    function test_public_internal() {
+        Public c = new Public();
+        uint[] memory a = new uint[](200);
+        a[10] = 8;
+        c.using_public(a);
+    }
+    
+}
