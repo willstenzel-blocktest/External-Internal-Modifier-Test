2835d050206e9b45c7411426482753c434aa95c5
willstenzel-blocktest <46688425+willstenzel-blocktest@users.noreply.github.com>
2019-01-25 16:01:26 -0500

    Update Public.sol

--- Public.sol
+++ Public.sol
@@ -7,22 +7,3 @@ contract Public {
     }
 }
 
-contract Test_Public_Internal is Public {
-    
-    function test_public_internal() {
-        uint[] memory a = new uint[](200);
-        a[10] = 8;
-        using_public(a);
-    }
-}
-    
-contract Test_Public_External {
-    
-    function test_public_external() {
-        Public c = new Public();
-        uint[] memory a = new uint[](200);
-        a[10] = 8;
-        c.using_public(a);
-    }
-    
-}
